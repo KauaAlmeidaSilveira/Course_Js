f51b451cdb10d8ceeb02007491871eadd88cbfbc
KauaAlmeidaSilveira <kauiis.a.silveira@gmail.com>
2022-01-27 14:01:29 -0300

    ThisBind2

--- Function/ThisBind.js
+++ Function/ThisBind.js
@@ -16,3 +16,32 @@ falar()
 
 const falarDePessoa = pessoa.falar.bind(pessoa) //BIND é responsavel por amarrar o 'this' em um determinado objeto
 falarDePessoa()
+
+
+console.log("------------------------------------------------")
+
+//PRIMEIRA FORMA
+
+// function pessoa1 (){
+//     this.idade = 0
+//
+//     setInterval(function(){
+//         this.idade++
+//         console.log(this.idade)
+//     }.bind(this), 1000)
+//
+// }
+
+//SEGUNDA FORMA
+
+function pessoa1 (){
+    this.idade = 0
+    const self = this
+    
+    setInterval(function(){
+        self.idade++
+        console.log(self.idade)
+    }, 1000)
+}
+
+new pessoa1
