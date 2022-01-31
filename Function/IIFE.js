75034e2f24b1498e8d2c9dcfafe9c9b77a6b1b79
KauaAlmeidaSilveira <kauiis.a.silveira@gmail.com>
2022-01-31 13:22:54 -0300

    IIFE

---
+++ Function/IIFE.js
@@ -0,0 +1,12 @@
+// IIFE -> expressão imediatamente invocada
+
+// PARA UTILIZAR O IIFE É NECESSARIO COLOCAR TODA A EXPRESSÃO ENTRE 
+// PARENTESES E EM SEGUINDA COLOCAR UM PAR DE PARENTESES, DESTA FORMA: (EXPRESSÃO)()
+
+// (function(){
+//     console.log('Será executado na hora!')
+//     console.log('Foge do escopo mais abrangente')
+// })()
+
+
+( () => {console.log('Será executado na hora!')} )()
